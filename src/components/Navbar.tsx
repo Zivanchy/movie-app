b53f6346d15b7a183c010cd35b32287d33881ce4
Zivanchy <zivanmadjarev@gmail.com>
2022-07-07 23:53:52 +0200

    Changed Logo to be a Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Nav from 'react-bootstrap/Nav';
 
@@ -6,7 +6,13 @@ const Navbar = () => {
   return (
     <Nav className="row align-items-center justify-content-between flex-nowrap stick-top bg-secondary">
       <Nav.Item className="col-auto text-light mx-5" as="h1">
-        Movie App
+        <Link
+          to="/"
+          className="link-warning"
+          style={{ textDecoration: 'none' }}
+        >
+          Movie App
+        </Link>
       </Nav.Item>
       <Nav.Item className="col-6 ml-auto"></Nav.Item>
     </Nav>
